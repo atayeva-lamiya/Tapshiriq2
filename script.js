dfb87ef99ecdcb4085c3b477ddb82f45db1ef31d
atayeva-lamiya <a.lamiya.a@mail.ru>
2021-02-22 19:48:59 +0400

    Tapshiriq

---
+++ script.js
@@ -0,0 +1,15 @@
+var Input = document.querySelectorAll("input");
+var Button = document.querySelectorAll("button");
+
+Button[0].onclick = function () {
+    console.log(Input[0].value);
+}
+Button[1].onclick = function () {
+    console.log(Input[1].value);
+}
+Button[2].onclick = function () {
+    console.log(Input[2].value);
+}
+Button[3].onclick = function () {
+    console.log(Input[3].value);
+}
